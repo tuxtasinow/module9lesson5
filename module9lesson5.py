151b66375c498f60d8c2252c8dc115ce47598f96
Bakhtiyar Tukhtasinov <144336544+tuxtasinow@users.noreply.github.com>
2024-09-30 15:05:44 +0500

    Add files via upload

--- module9lesson5.py
+++ module9lesson5.py
@@ -4,22 +4,24 @@ class StepValueError(ValueError):
 
 class Iterator:
     def __init__(self, start, stop, step=1):
+        if step == 0:
+            raise StepValueError('шаг не может быть равен 0')
+        
         self.start = start
         self.stop = stop
         self.step = step
         self.pointer = start
-        if step == 0:
-            raise StepValueError('шаг не может быть равен 0')
 
     def __iter__(self):
         self.pointer = self.start
         return self
 
     def __next__(self):
-        if self.step > 0 and self.pointer >= self.stop or self.step < 0 and self.pointer <= self.stop:
+        if (self.step > 0 and self.pointer > self.stop) or (self.step < 0 and self.pointer < self.stop):
             raise StopIteration
+        curret_value = self.pointer
         self.pointer += self.step
-        return self.pointer
+        return curret_value
 
 
 try:
